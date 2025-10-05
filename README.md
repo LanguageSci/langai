7f4c54e9b9a1c23cf9f4505ee25bf3befd450a49
LanguageSci <languagesci2025@gmail.com>
2025-10-05 17:14:12 +0800

    'init'

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 ## 环境要求
 ### 一、本地化AI部署
 （1）下载 ollma 平台：https://ollama.com/download
+（2）安装成功后测试：ollama --version
+（3）下载 deepseek 模型：ollama run deepseek-r1:1.5b
+（4）下载完成自动运行 r1 测试模型，可使用默认地址访问运行状态：http://127.0.0.1:11434/
+（5）并修改 AI SDK provider
+    import { ollama } from 'ollama-ai-provider-v2';
+    ...
+    model: ollama("deepseek-r1:1.5b")
+    ...
+
+
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,10 +1,12 @@
 import { openai } from "@ai-sdk/openai";
 import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { ollama } from 'ollama-ai-provider-v2';
+
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
   const result = streamText({
-    model: openai("gpt-5-nano"),
+    model: ollama("deepseek-r1:1.5b"),
     messages: convertToModelMessages(messages),
   });
 
